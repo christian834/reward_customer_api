d56b3fa83fc2af3801e54c5a70a6e670582d5bdb
christian834 <christian.londono@pyxisportal.co>
2021-10-14 18:19:50 -0500

    Create project

---
+++ src/test/java/com/rewardCustomer/RewardCustomerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rewardCustomer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RewardCustomerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
